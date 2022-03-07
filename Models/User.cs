afeee34b6d80c6f12e15d3034c131fca4c740979
Wenjiu Wang <vanjor1014@gmail.com>
2022-03-07 12:17:13 -0500

    Add default address to user model

--- Models/User.cs
+++ Models/User.cs
@@ -5,12 +5,13 @@ public class User {
     public string Email { get; set; }
     public string Password { get; set; }
     public IList<CartItem> Cart { get; set; }
-
-    public User(string Id, string Name, string Email, string Password, IList<CartItem> Cart) {
+    public string DefaultAddress { get; set;}
+    public User(string Id, string Name, string Email, string Password, IList<CartItem> Cart, string DefaultAddress) {
         this.Id = Id;
         this.Name = Name;
         this.Email = Email;
         this.Password = Password;
         this.Cart = new List<CartItem>(Cart);
+        this.DefaultAddress = DefaultAddress;
     }
 }
