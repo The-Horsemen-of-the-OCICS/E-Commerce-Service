c8809274026f32fd8883fa5a3bb3fc2dac58222d
Luwei Cai <90882967+lcai006@users.noreply.github.com>
2022-03-31 03:20:10 -0400

    Update DB query for sort by date

--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -5,13 +5,7 @@ using MongoDB.Driver;
 namespace ecommerceapp.services;
 public class OrderService {
     private readonly IMongoCollection<Order> _orders;
-    private List<Order> orders = new List<Order> () {
-        new Order("1", "1", "User1", "user1@gmail.com", new List<CartItem>(){new CartItem("1", "Three-Pack Off-White Jersey T-Shirts", 15 ,"placeholder", 2)}, 30, "2022-1-12", "75 Laurier Ave. E, Ottawa, ON, Canada"),
-        new Order("2", "1", "User1", "user1@gmail.com", new List<CartItem>(){new CartItem("1", "Black Relaxed Sweater", 5 ,"placeholder", 2), new CartItem("2", "JW ANDERSON Glass Stand", 25 ,"https://i.postimg.cc/j5kCTjnV/16340030929e7b3bd5c75857d1c040c639acc70476-thumbnail-900x.jpg", 1)}, 35, "2022-1-20", "75 Laurier Ave. E, Ottawa, ON, Canada"),
-        new Order("3", "2", "User2", "user2@gmail.com", new List<CartItem>(){new CartItem("2", "JW ANDERSON Glass Stand", 25 ,"https://i.postimg.cc/j5kCTjnV/16340030929e7b3bd5c75857d1c040c639acc70476-thumbnail-900x.jpg", 1)}, 25, "2022-1-25", "75 Laurier Ave. E, Ottawa, ON, Canada")
-
-    };
-
+    
     public OrderService(IOptions<DatabaseSettings> databaseSettings) {
         var settings = MongoClientSettings.FromConnectionString(databaseSettings.Value.ConnectionString);
         settings.ServerApi = new ServerApi(ServerApiVersion.V1);
@@ -28,8 +22,7 @@ public class OrderService {
 
 /// Get all orders
     public async Task<List<Order>> GetAsync() {
-        return orders;
-        return await _orders.Find(_ => true).ToListAsync();
+        return await _orders.Find(_ => true).SortByDescending(x => x.OrderDate).ToListAsync();
     }
 
 /// Get a specific order by id
--- Services/ResponseService.cs
+++ Services/ResponseService.cs
@@ -5,12 +5,6 @@ using MongoDB.Driver;
 namespace ecommerceapp.services;
 public class ResponseService {
     private readonly IMongoCollection<Response> _responses;
-    private List<Response> responses = new List<Response> () {
-        new Response("1", "1", "0", "Yes it is free", "2022-1-13", 1),
-        new Response("2", "1", "0", "Yes it is free!!!", "2022-1-13", 3),
-        new Response("3", "2", "0", "Yes it does", "2022-1-13", 2),
-        new Response("4", "2", "0", "I don't know", "2022-1-13", 1)
-    };
 
     public ResponseService(IOptions<DatabaseSettings> databaseSettings) {
         var settings = MongoClientSettings.FromConnectionString(databaseSettings.Value.ConnectionString);
@@ -28,12 +22,12 @@ public class ResponseService {
 
 /// Get all responses in database
     public async Task<List<Response>> GetAsync() {
-        return await _responses.Find(_ => true).ToListAsync();
+        return await _responses.Find(_ => true).SortByDescending(x => x.Date).ToListAsync();
     }
 
 /// Get all responses for a question by question id
     public async Task<List<Response>> GetByQuestionAsync(string questionId) {
-        return await _responses.Find<Response>(x => x.QuestionId == questionId).ToListAsync();
+        return await _responses.Find<Response>(x => x.QuestionId == questionId).SortByDescending(x => x.Date).ToListAsync();
     }
 
 /// Get a specific response by id
